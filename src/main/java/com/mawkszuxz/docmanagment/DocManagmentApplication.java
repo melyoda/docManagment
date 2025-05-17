3f1ea609f1218503567ddac5cc6b0f60171185e5
melyoda <amv123doom@gmail.com>
2025-05-17 12:30:08 +0300

    Initial commit

---
+++ src/main/java/com/mawkszuxz/docmanagment/DocManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.mawkszuxz.docmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocManagmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocManagmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mawkszuxz/docmanagment/DocManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mawkszuxz.docmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
